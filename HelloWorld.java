9a997e13506a16a5693e9380e86703f25cdbbf56
Aaron Kelly <70290539+akelly24@users.noreply.github.com>
2020-11-20 17:45:55 -0500

    Add files via upload

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,36 @@
-package HelloWorld;
 
+package HelloWorld;
+import java.util.Scanner;
+import java.lang.Math;
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World, this is Aaron!");
 
+		Scanner scnr = new Scanner(System.in);
+		
+	    double wallHeight;
+	    double wallWidth;
+	    double wallArea;
+	      
+	    wallHeight = scnr.nextDouble();
+	    wallWidth = scnr.nextDouble();
+	    wallArea = wallHeight * wallWidth;
+	    scnr.close();
+	    System.out.println("Enter wall height (feet):");
+	      //System.out.println("" + wallHeight);
+	    System.out.println("Enter wall width (feet):");
+	     //System.out.println("" + wallWidth);
+	    System.out.println("Wall area: " + wallArea + " square feet");
+	      
+	    double wallPaint = wallArea / 350.0;
+	      
+	    System.out.println("Paint needed: " + wallPaint + " gallons");
+	      
+	      
+	    System.out.println("Cans needed: " + Math.round(wallPaint) + " can(s)");
+	
 	}
 
 }
